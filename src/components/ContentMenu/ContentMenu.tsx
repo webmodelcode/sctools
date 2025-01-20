eee0805546cee7134425b32bf747d34146802978
Juan Pablo Leon <juanleon8581@gmail.com>
2025-01-20 02:47:23 -0500

    Enhance ContentMenu component with additional styling and integrate it into the main application

--- src/components/ContentMenu/ContentMenu.tsx
+++ src/components/ContentMenu/ContentMenu.tsx
@@ -2,18 +2,18 @@ import {
   Accordion,
   AccordionContent,
   AccordionItem,
-  AccordionTrigger
-} from "@/components/ui/accordion"
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export const ContentMenu = () => {
   return (
-    <div className="sct-max-w-20 sct-min-w-20 sct-fixed sct-bottom-0 sct-left-10">
+    <div className="sct-max-w-20 sct-min-w-20 sct-fixed sct-bottom-0 sct-left-10 sct-bg-gray-300/70 sct-z-[9999] sct-text-black sct-rounded-t-md">
       <Accordion type="single" collapsible role="accordion">
         <AccordionItem value="item-1">
-          <AccordionTrigger className="sct-justify-around" />
+          <AccordionTrigger className="sct-justify-around sct-py-1.5 [&>svg]:sct-text-red" />
           <AccordionContent>Accordion Content</AccordionContent>
         </AccordionItem>
       </Accordion>
     </div>
-  )
-}
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ContentMenu, Popup } from "./components";
 import "./index.css";
-import { Popup } from "./components";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Popup />
+    <ContentMenu />
   </StrictMode>
 );
