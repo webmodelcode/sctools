e2468b90e5927e4671965785caa84997c6eac6ee
jpleon <juanleon8581@gmail.com>
2025-01-20 22:01:33 -0500

    start localstorage test

---
+++ src/hooks/useLocalStorage/__test__/useLocalStorage.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
+import { useLocalStorage } from "../useLocalStorage";
+
+describe("useLocalStorage", () => {
+  it("should use chrome.storage.local.set to save data", async () => {
+    const { setItem } = useLocalStorage();
+    vi.spyOn(chrome.storage.local, "set").mockResolvedValue(undefined);
+    setItem("testKey", "testValue");
+
+    expect(chrome.storage.local.set).toHaveBeenCalledWith({
+      testKey: "testValue",
+    });
+  });
+});
