f255846e9acd38cde1f3247966756d74b6ae220d
Juan Pablo Leon <juanleon8581@gmail.com>
2025-01-23 02:57:10 -0500

    remove no necesary component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ContentMenu, Popup } from "./components";
+import { Popup } from "./components";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
