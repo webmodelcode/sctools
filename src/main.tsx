34876f63713fd8d98688c969be6074c1deda4421
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-02 18:24:04 -0500

    remove unnecesary use of components

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Popup } from "./components";
 import "./index.css";
-import { QuickMessagesMenu } from "./components/QuickMessages/QuickMessagesMenu/QuickMessagesMenu";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Popup />
-    {/* <QuickMessagesMenu /> */}
-    <input type="text" />
   </StrictMode>
 );
