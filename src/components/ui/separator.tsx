37cfbc62d4d510eead92dd98a04005e8f5fb082a
Juan Pablo Leon <juanleon8581@gmail.com>
2025-02-04 00:18:57 -0500

    Add shadCn Separator Component

---
+++ src/components/ui/separator.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+import { cn } from "@/lib/utils"
+
+const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <SeparatorPrimitive.Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "sct-shrink-0 sct-bg-border",
+        orientation === "horizontal" ? "sct-h-[1px] sct-w-full" : "sct-h-full sct-w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separator.displayName = SeparatorPrimitive.Root.displayName
+
+export { Separator }
