00a020b2d1fd9ab2c9a329f2801d8f7ba79627cf
Juan Pablo Leon <juanleon8581@gmail.com>
2025-07-14 18:01:13 -0500

    remove all old structure for migrate to wxt framework

--- src/services/index.ts
+++
@@ -1,8 +0,0 @@
-export {
-  getQuickMessages,
-  addQuickMessage,
-  updateQuickMessage,
-  deleteQuickMessage,
-  clearQuickMessages,
-} from "@/services/quickMessagesService";
-export type { QuickMessage as QuickMessageType } from "@/services/quickMessagesService";
